77823857bd8eb5b1fc482581ecbfa18e2edfa887
Xiomara Gonzalez <94167532+xiomaragonzalezgonzalez@users.noreply.github.com>
2025-11-26 17:04:51 +0000

    Add outputs for VPC ID and public subnet IDs

---
+++ aws/vpc/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
