2360b44dde99ec302ad4f5acf6cf4e9f3e004d48
Xiomara Gonzalez <94167532+xiomaragonzalezgonzalez@users.noreply.github.com>
2025-11-26 17:03:14 +0000

    Add VPC and public subnet resources in Terraform

---
+++ aws/vpc/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-${count.index}"
+  }
+}
