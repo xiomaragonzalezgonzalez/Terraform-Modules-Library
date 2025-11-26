b65cb6ef519c499974287ccc227164b371483544
Xiomara Gonzalez <94167532+xiomaragonzalezgonzalez@users.noreply.github.com>
2025-11-26 17:03:50 +0000

    Add variables for VPC configuration
    Define variables for VPC configuration including name, CIDR block, and public subnets.

---
+++ aws/vpc/aws/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "Name prefix for the VPC"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
