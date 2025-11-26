14185a65e7dc95048719916cef41dbaab7e63f9e
Xiomara Gonzalez <94167532+xiomaragonzalezgonzalez@users.noreply.github.com>
2025-11-26 19:42:49 +0000

    Add bucket ARN and domain name outputs

--- aws/s3/outputs.tf
+++ aws/s3/outputs.tf
@@ -1,3 +1,14 @@
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.this.arn
+}
+
 output "bucket_id" {
-  value = aws_s3_bucket.this.id
+  description = "ID (name) of the S3 bucket"
+  value       = aws_s3_bucket.this.id
+}
+
+output "bucket_domain_name" {
+  description = "S3 bucket domain name"
+  value       = aws_s3_bucket.this.bucket_domain_name
 }
